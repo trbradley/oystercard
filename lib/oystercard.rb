f045bab3aea9da5eaa27a3d4582387f33cc9f825
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-30 18:35:02 +0000

    ch-05 can top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
 
-attr_reader :balance
+attr_accessor :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(money)
+    @balance += money
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,10 @@ describe Oystercard do
     expect(oystercard.balance).to eq 0
   end
 
+  it 'can top up an oystercard' do
+    oystercard.balance = 10
+    oystercard.top_up(20)
+    expect(oystercard.balance).to eq 30
+  end
+
 end
